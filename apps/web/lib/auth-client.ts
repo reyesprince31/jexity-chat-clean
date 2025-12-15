ee817b31786ed1ff82c410b04b2d7747271cd9f2
Prince Reyes <reyes.prince31@gmail.com>
2025-12-15 23:02:09 +0800

    fix: web better-auth types infer

--- apps/web/lib/auth-client.ts
+++ apps/web/lib/auth-client.ts
@@ -1,11 +1,11 @@
-import type { auth } from "@repo/auth";
 import { adminClient, inferAdditionalFields, organizationClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
+import type { auth } from "@repo/auth";
 
-export const authClient: ReturnType<typeof createAuthClient> = createAuthClient({
+export const authClient = createAuthClient({
 	plugins: [
+		inferAdditionalFields<typeof auth>(),
 		adminClient(),
 		organizationClient(),
-		inferAdditionalFields<typeof auth>()
 	],
 });
